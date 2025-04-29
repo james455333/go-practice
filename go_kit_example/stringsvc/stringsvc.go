a92b9eb95e7c26ce3958a187bab7d2711712fc88
james455333 <james455333@gmail.com>
2025-04-30 00:53:17 +0800

    feat(go-kit): create logging middleware

--- go_kit_example/stringsvc/stringsvc.go
+++ go_kit_example/stringsvc/stringsvc.go
@@ -4,28 +4,35 @@ import (
 	"context"
 	"encoding/json"
 	httptransport "github.com/go-kit/kit/transport/http"
-	"log"
+	"github.com/go-kit/log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	svc := stringService{}
+	logger := log.NewLogfmtLogger(os.Stderr)
 
+	svc := stringService{}
+	uppercase := makeUppercaseEndpoint(svc)
+	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
 	uppercaseHandler := httptransport.NewServer(
-		makeUppercaseEndpoint(svc),
+		uppercase,
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
 
+	count := makeCountEndpoint(svc)
+	count = loggingMiddleware(log.With(logger, "method", "count"))(count)
 	countHandler := httptransport.NewServer(
-		makeCountEndpoint(svc),
+		count,
 		decodeCountRequest,
 		encodeResponse,
 	)
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	logger.Log("msg", "listening on :8080")
+	logger.Log("msg", http.ListenAndServe(":8080", nil))
 }
 
 func decodeCountRequest(ctx context.Context, request *http.Request) (interface{}, error) {
