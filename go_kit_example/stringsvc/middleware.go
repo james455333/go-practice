4f35d66951afb6e65e94d4d014412121f7624057
james455333 <james455333@gmail.com>
2025-05-03 23:25:24 +0800

    feat(stringsvc): modify stringsvc2 with adding instrument middleware of prometheus

--- go_kit_example/stringsvc/middleware.go
+++ go_kit_example/stringsvc/middleware.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-kit/kit/metrics"
 	"github.com/go-kit/log"
 	"time"
 )
 
-var _ StringService = (*loggingMiddleware)(nil)
+var (
+	_ StringService = (*instrumentingMiddleware)(nil)
+	_ StringService = (*loggingMiddleware)(nil)
+)
 
 type loggingMiddleware struct {
 	logger log.Logger
@@ -35,6 +40,36 @@ func (mw loggingMiddleware) Count(s string) (n int) {
 			"n", n,
 			"took", time.Since(begin),
 		)
+	}(time.Now())
+
+	n = mw.next.Count(s)
+	return
+}
+
+type instrumentingMiddleware struct {
+	requestCount   metrics.Counter
+	requestLatency metrics.Histogram
+	countResult    metrics.Histogram
+	next           StringService
+}
+
+func (mw instrumentingMiddleware) UpperCase(s string) (output string, err error) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "uppercase", "error", fmt.Sprint(err != nil)}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	output, err = mw.next.UpperCase(s)
+	return
+}
+
+func (mw instrumentingMiddleware) Count(s string) (n int) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "count", "error", "false"}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+		mw.countResult.Observe(float64(n))
 	}(time.Now())
 
 	n = mw.next.Count(s)
--- go_kit_example/stringsvc/stringsvc.go
+++ go_kit_example/stringsvc/stringsvc.go
@@ -3,17 +3,44 @@ package main
 import (
 	"context"
 	"encoding/json"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/go-kit/log"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"os"
 )
 
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
+
+	fieldKey := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_count",
+		Help:      "Number of request received.",
+	}, fieldKey)
+
+	requestLatency := kitprometheus.NewHistogramFrom(stdprometheus.HistogramOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKey)
+
+	counterResult := kitprometheus.NewHistogramFrom(stdprometheus.HistogramOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:      "counter_result",
+		Help:      "the result of each count result",
+	}, fieldKey)
+
 	var svc StringService
 	svc = stringService{}
 	svc = loggingMiddleware{logger, svc}
+	svc = instrumentingMiddleware{requestCount, requestLatency, counterResult, svc}
 
 	uppercaseHandler := httptransport.NewServer(
 		makeUppercaseEndpoint(svc),
@@ -24,6 +51,7 @@ func main() {
 	var count StringService
 	count = stringService{}
 	count = loggingMiddleware{logger, count}
+	count = instrumentingMiddleware{requestCount, requestLatency, counterResult, count}
 	countHandler := httptransport.NewServer(
 		makeCountEndpoint(count),
 		decodeCountRequest,
@@ -32,6 +60,7 @@ func main() {
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
+	http.Handle("/metrics", promhttp.Handler())
 	logger.Log("msg", "listening on :8080")
 	logger.Log("msg", http.ListenAndServe(":8080", nil))
 }
